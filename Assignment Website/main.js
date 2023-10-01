1099e03f3c4afea5ac30047af9ebf8adccdcb97a
Shivangi <126843935+shivangi2920@users.noreply.github.com>
2023-10-02 01:46:38 +0530

    Add files via upload

---
+++ Assignment Website/main.js
@@ -0,0 +1,21 @@
+// window.addEventListener('scroll', function () {
+//     let navbar = document.getElementById("nav");
+    
+   
+ 
+   
+  
+   
+  
+//     if (window.scrollY > 50) {
+//       navbar.classList.add("scrolled");
+    
+  
+//     } else {
+//       navbar.classList.remove("scrolled");
+//       }
+     
+  
+//     });
+
+    
